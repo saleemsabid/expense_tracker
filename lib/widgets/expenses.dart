25734a74d641d1d6729c12c29838b1cca36c8bb0
saleemsabid <saleemsabid@gmail.com>
2023-08-06 21:36:09 +0530

    added the initial structure to the app

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,58 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Trip to Tirur',
+      amount: 9.69,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+    Expense(
+      title: 'Flutter Course',
+      amount: 69.69,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Food',
+      amount: 169.69,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Expense Tracker'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+              ],
+            ),
+          ),
+        ));
+  }
+}
