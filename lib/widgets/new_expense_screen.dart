84fabbdeea1e13bf7de88282642f88a6a2184449
saleemsabid <saleemsabid@gmail.com>
2023-08-07 22:30:50 +0530

    added the framework of the form

--- lib/widgets/new_expense_screen.dart
+++ lib/widgets/new_expense_screen.dart
@@ -13,31 +13,63 @@ class _NewExpenseScreenState extends State<NewExpenseScreen> {
     return Center(
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
-        child: Column(children: [
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
           const Text('Add Your New Expenses Here'),
-          const SizedBox(
-            height: 16,
-          ),
-          const Expanded(
-            child: TextField(
-              decoration: InputDecoration(label: Text('Title')),
-            ),
-          ),
           // const SizedBox(
           //   height: 16,
           // ),
-          Expanded(
-            child: Row(
-              children: [
-                const Text('\$'),
-                const Expanded(child: TextField()),
-                const Text('Date:'),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.calendar_month),
+          const TextField(
+            decoration: InputDecoration(label: Text('Title')),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            children: [
+              const Expanded(
+                  child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    label: Text(
+                      'Amount',
+                    ),
+                    prefix: Text('\$')),
+              )),
+              const Text('Date:'),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.calendar_month),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            children: [
+              const Text('Category'),
+              const SizedBox(
+                width: 16,
+              ),
+              DropdownButton(items: const [
+                DropdownMenuItem(
+                  child: Text('Work'),
                 ),
-              ],
-            ),
+              ], onChanged: (value) {})
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text('cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Save'),
+              )
+            ],
           )
         ]),
       ),
